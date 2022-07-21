1596b5a48b0cc6a5aa1add9af7e6aa674592ca91
TalgatBatyrov <90198155+TalgatBatyrov@users.noreply.github.com>
2022-07-21 17:31:53 +0600

    Connected localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_flutter_app/widgets/home.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('ky', 'KG'),
+        Locale('ru', 'RU'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('ru', 'RU'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
